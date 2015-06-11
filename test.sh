75682d6f76ae3b733b25a4ffd5269ccae9b70153
Colm Harte <colm.harte@nearform.com>
2015-06-11 12:07:52 +0100

    Initial project

---
+++ test.sh
@@ -0,0 +1,8 @@
+# Only install seneca to run tests.
+# There is no explicit dependency either in production or development
+# because this plugin is itself a dependency of the seneca module.
+
+if [ ! -d "./node_modules/seneca" ]; then
+  npm install seneca@plugin
+fi
+./node_modules/.bin/mocha test/*.test.js
